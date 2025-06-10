7287f3b0b2bfb4b1a4832a661df4147a4ac595b6
이승환 <seaung13@gmail.com>
2025-06-10 09:38:21 +0900

    test: [studycafe] InputHandler.getPassTypeSelectingUserAction() 패스 타입 선택지 입력 테스트 추가

--- src/test/java/cleancode/studycafe/tobe/io/InputHandlerTest.java
+++ src/test/java/cleancode/studycafe/tobe/io/InputHandlerTest.java
@@ -12,6 +12,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InputHandlerTest {
 
+    @DisplayName("사용자가 패스 타입에 맞는 선택지를 입력할 경우 해당하는 StudyCafePassType Enum을 반환한다.")
+    @Test
+    void getPassTypeSelectingUserAction() {
+        // given
+        InputHandler inputHandler = new InputHandler();
+        String userInput = "1";
 
+        // when
+        StudyCafePassType passType = inputHandler.getPassTypeSelectingUserAction(userInput);
+
+        // then
+        assertThat(passType).isEqualTo(StudyCafePassType.HOURLY);
+    }
+
+    @DisplayName("사용자가 잘못된 패스 타입 선택지를 입력할 경우 예외가 발생한다.")
+    @Test
+    void getPassTypeSelectingUserAction_exception() {
+        // given
+        InputHandler inputHandler = new InputHandler();
+        String userInput = "아무거나";
+
+        // when then
+        assertThatThrownBy(() -> inputHandler.getPassTypeSelectingUserAction(userInput))
+            .isInstanceOf(AppException.class)
+            .hasMessage("잘못된 입력입니다.");
+    }
 
 }
