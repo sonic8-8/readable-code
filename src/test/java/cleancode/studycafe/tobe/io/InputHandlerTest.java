df039ccb1e7cb935399070ba4f72075981075f9c
이승환 <seaung13@gmail.com>
2025-06-10 09:35:48 +0900

    feat: [studycafe] InputHandler.getPassTypeSelectingUserAction(String) 단위 테스트 지원 메서드 추가

---
+++ src/test/java/cleancode/studycafe/tobe/io/InputHandlerTest.java
@@ -0,0 +1,17 @@
+package cleancode.studycafe.tobe.io;
+
+import cleancode.studycafe.tobe.exception.AppException;
+import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputHandlerTest {
+
+
+
+}
