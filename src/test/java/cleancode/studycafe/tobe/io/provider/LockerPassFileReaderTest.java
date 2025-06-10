117553fd767d2ed2079d879a2996be6ccbef10f6
이승환 <seaung13@gmail.com>
2025-06-10 12:03:48 +0900

    test: [studycafe] LockerPassFileReader.getLockerPasses() 정상/예외 케이스 테스트 추가

--- src/test/java/cleancode/studycafe/tobe/io/provider/LockerPassFileReaderTest.java
+++ src/test/java/cleancode/studycafe/tobe/io/provider/LockerPassFileReaderTest.java
@@ -9,11 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -21,5 +19,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LockerPassFileReaderTest {
 
+    @DisplayName("CSV 파일을 읽고 StudyCafeLockerPasses를 반환할 수 있다.")
+    @Test
+    void getLockerPasses() throws IOException {
+        // given
+        LockerPassFileReader lockerPassFileReader = new LockerPassFileReader();
+
+        File tempFile = File.createTempFile("temp", ".csv");
+        tempFile.deleteOnExit();
+
+        String contents = "FIXED,4,10000\n" +
+            "FIXED,12,30000";
+        Files.writeString(tempFile.toPath(), contents);
+
+        String path = tempFile.getPath();
+
+        SeatPassFileReader seatPassFileReader = new SeatPassFileReader();
+        StudyCafeSeatPasses seatPasses = seatPassFileReader.getSeatPasses();
+        List<StudyCafeSeatPass> seatPassList = seatPasses.findPassBy(StudyCafePassType.FIXED);
+        StudyCafeSeatPass fixedSeatPass = seatPassList.get(0);
+
+        // when
+        StudyCafeLockerPasses lockerPasses = lockerPassFileReader.getLockerPasses(path);
+
+        // then
+        StudyCafeLockerPass lockerPass = lockerPasses.findLockerPassBy(fixedSeatPass)
+            .orElseThrow(() -> new IllegalArgumentException("고정 좌석권이 아닙니다."));
+        assertThat(lockerPass.getPassType()).isEqualTo(StudyCafePassType.FIXED);
+        assertThat(lockerPass.getPrice()).isEqualTo(fixedSeatPass.getPrice());
+    }
+
+    @DisplayName("존재하지 않는 파일 경로를 읽을 경우 RuntimeException이 발생한다.")
+    @Test
+    void getLockerPasses_exception() {
+        // given
+        LockerPassFileReader lockerPassFileReader = new LockerPassFileReader();
+
+        // when then
+        assertThatThrownBy(() -> lockerPassFileReader.getLockerPasses(""))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("파일을 읽는데 실패했습니다.");
+    }
 
 }
